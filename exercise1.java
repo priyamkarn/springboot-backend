40856d28e627ae920f60ef204fa00fbaa0bdfd1c
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2024-01-04 04:46:19 +0530

    Create exercise1.java

---
+++ exercise1.java
@@ -0,0 +1,22 @@
+package com.scalar.springboot1project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+  /*
+  activates http request
+  */
+public class Taskcon {
+/*
+  http get request
+  /*
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "priyam is a good boy";
+    }
+
+}
