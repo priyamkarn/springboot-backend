ab0005671277fc184b980d20bb341e30dd2a66d9
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2024-01-04 04:47:13 +0530

    Create springbootapplictation.java

---
+++ springbootapplictation.java
@@ -0,0 +1,15 @@
+package com.scalar.springboot1project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Springboot1Application.class, args);
+
+    }
+
+}
